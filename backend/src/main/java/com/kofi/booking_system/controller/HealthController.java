64663a1ec619d36068c82ac8b2af4dadf97667dc
Kofidhope <forsonbentum55@gmail.com>
2026-02-02 15:03:40 +0000

    Initial project setup with DB connection

---
+++ backend/src/main/java/com/kofi/booking_system/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.kofi.booking_system.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<String> check(){
+        return ResponseEntity.ok("Appointment System is up and running ✅");
+    }
+}
+
+
