c8967b55bf0c68bec312bd86dc678ff6d15db79b
Kofidhope <forsonbentum55@gmail.com>
2026-02-23 14:19:37 +0000

    implement create provider endpoint

--- backend/src/main/java/com/kofi/booking_system/providerService/controller/ProviderServiceController.java
+++ backend/src/main/java/com/kofi/booking_system/providerService/controller/ProviderServiceController.java
@@ -1,15 +1,21 @@
 package com.kofi.booking_system.providerService.controller;
 
+import com.kofi.booking_system.admin.dto.CreateProviderRequest;
 import com.kofi.booking_system.providerService.dto.CreateServiceRequest;
 import com.kofi.booking_system.providerService.dto.ProviderServiceResponse;
 import com.kofi.booking_system.providerService.model.ProviderService;
 import com.kofi.booking_system.providerService.service.ProviderServiceService;
+import com.kofi.booking_system.user.model.Role;
+import com.kofi.booking_system.user.model.User;
+import com.kofi.booking_system.user.repository.UserRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +24,8 @@ import org.springframework.web.bind.annotation.*;
 public class ProviderServiceController {
 
     private final ProviderServiceService providerService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/services")
     @PreAuthorize("hasRole('SERVICE_PROVIDER')")
@@ -36,6 +44,25 @@ public class ProviderServiceController {
             @RequestParam(defaultValue = "10") int size
     ) {
         return ResponseEntity.ok(providerService.getServicesByProvider(providerId,page,size));
+    }
+
+    @PostMapping("/create/providers")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> createProvider(@RequestBody @Valid CreateProviderRequest request) {
+
+        User provider = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .phoneNumber(request.getPhoneNumber())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.SERVICE_PROVIDER)
+                .enabled(true) // providers don't need OTP verification
+                .build();
+
+        userRepository.save(provider);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
