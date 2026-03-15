7581a2a5cb0ca7ea5aea38ac965248bafef5e2ea
Kofidhope <forsonbentum55@gmail.com>
2026-03-15 22:04:03 +0000

    added actuators

--- backend/src/main/java/com/kofi/booking_system/auth/security/SecurityConfig.java
+++ backend/src/main/java/com/kofi/booking_system/auth/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 //Define which endpoints are public
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 // THIS LINE IS IMPORTANT
