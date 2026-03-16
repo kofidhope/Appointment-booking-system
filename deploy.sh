c9cb0903337fb4a798d8317c5ba181c6fa0d6cd8
Kofidhope <forsonbentum55@gmail.com>
2026-03-16 19:53:03 +0000

    Update CI/CD pipeline and deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,17 @@
 
 set -e  # Stop script if any command fails
 
+echo "Navigating to app directory..."
+cd ~/app/Appointment-booking-system
+
+echo "Pulling latest code..."
+git pull origin main
+
 echo "Pulling latest images..."
 docker compose pull
 
 echo "Restarting containers..."
+docker compose down
 docker compose up -d
 
 echo "Deployment complete!"
