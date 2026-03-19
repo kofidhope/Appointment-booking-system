d8781a65240cd05d867c1a4e8dec5648552b9a1e
Kofidhope <forsonbentum55@gmail.com>
2026-03-19 13:42:51 +0000

    Fix Paystack webhook DTO to ignore unknown fields

--- backend/src/main/java/com/kofi/booking_system/payment/dto/PaystackWebhookData.java
+++ backend/src/main/java/com/kofi/booking_system/payment/dto/PaystackWebhookData.java
@@ -1,8 +1,10 @@
 package com.kofi.booking_system.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PaystackWebhookData {
     private String reference;  // "PAY12"
     private String status;     // "success"
--- backend/src/main/java/com/kofi/booking_system/payment/dto/PaystackWebhookEvent.java
+++ backend/src/main/java/com/kofi/booking_system/payment/dto/PaystackWebhookEvent.java
@@ -1,8 +1,10 @@
 package com.kofi.booking_system.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PaystackWebhookEvent {
     private String event;        // e.g. "charge.success"
     private PaystackWebhookData data;
